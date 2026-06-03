98e3a5276fbc608e2ce8c13d8c1dd13586bef634
nathanctay <63444651+nathanctay@users.noreply.github.com>
2026-06-03 16:12:32 -0600

    use unix domain socker for getting audio fingerprint

--- middleware/main.go
+++ middleware/main.go
@@ -6,8 +6,12 @@ import (
 	"io"
 	"log"
 	"math/rand/v2"
+	"net"
 	"net/http"
+	"os"
+	"os/signal"
 	"sync"
+	"syscall"
 	"time"
 )
 
@@ -31,43 +35,102 @@ func main() {
 	mux := http.NewServeMux()
 	broker := newBroker()
 
-	mux.HandleFunc("POST /identify", handleIdentify(broker))
+	// mux.HandleFunc("POST /identify", handleIdentify(broker))
 	mux.HandleFunc("GET /events", handleSSE(broker))
+
+	os.Remove("/tmp/shazam.sock")
+	socket, err := net.Listen("unix", "/tmp/shazam.sock")
+	if err != nil {
+		log.Fatal("Could not connect to Audio Worker\n", "err: ", err)
+	}
+	go handleIdentify(broker, socket)
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		os.Remove("/tmp/shazam.sock")
+		os.Exit(1)
+	}()
 
 	fmt.Println("Server running on :8080")
 	log.Fatal(http.ListenAndServe(":8080", mux))
 }
 
-func handleIdentify(broker *SSEBroker) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		b, err := io.ReadAll(r.Body)
-		if err != nil || len(b) < 1 {
-			http.Error(w, "bad request body", http.StatusBadRequest)
-			return
-		}
-
-		var req FingerprintData
-		err = json.Unmarshal(b, &req)
+func handleIdentify(broker *SSEBroker, socket net.Listener) {
+	for {
+		conn, err := socket.Accept()
 		if err != nil {
-			http.Error(w, "error parsing request JSON", http.StatusBadRequest)
+			log.Println("socket accept error:", err)
 			return
 		}
 
-		// TODO: identify song
+		go func(conn net.Conn) {
+			defer conn.Close()
 
-		sampleSong := Track{
-			Title:         "Never Gonna Give you up",
-			Artist:        "Rick Astley",
-			DurationS:     rand.IntN(500), // this is just for a tiny bit of variety in the samples
-			Album:         "Off the wall",
-			AlbumCoverURL: "https://upload.wikimedia.org/wikipedia/en/1/1c/Rick_Astley_-_Whenever_You_Need_Somebody.png",
-		}
+			data, err := io.ReadAll(conn)
+			if err != nil {
+				log.Println("error reading from connection:", err)
+				return
+			}
 
-		// broadcast the value to the frontend. returning it to the rust layer making the call will do us no good
-		broker.publish(sampleSong)
-		w.WriteHeader(http.StatusOK)
+			var req FingerprintData
+			err = json.Unmarshal(data, &req)
+			if err != nil {
+				log.Println("error parsing request JSON:", err)
+				return
+			}
+
+			// TODO: identify song
+
+			// fmt.Println(req)
+
+			sampleSong := Track{
+				Title:         "Never Gonna Give you up",
+				Artist:        "Rick Astley",
+				DurationS:     rand.IntN(500), // this is just for a tiny bit of variety in the samples
+				Album:         "Off the wall",
+				AlbumCoverURL: "https://upload.wikimedia.org/wikipedia/en/1/1c/Rick_Astley_-_Whenever_You_Need_Somebody.png",
+			}
+
+			// broadcast the value to the frontend. returning it to the rust layer making the call will do us no good
+			broker.publish(sampleSong)
+		}(conn)
+
 	}
 }
+
+// func handleIdentify(broker *SSEBroker) http.HandlerFunc {
+
+// 	return func(w http.ResponseWriter, r *http.Request) {
+// 		b, err := io.ReadAll(r.Body)
+// 		if err != nil || len(b) < 1 {
+// 			http.Error(w, "bad request body", http.StatusBadRequest)
+// 			return
+// 		}
+
+// 		var req FingerprintData
+// 		err = json.Unmarshal(b, &req)
+// 		if err != nil {
+// 			http.Error(w, "error parsing request JSON", http.StatusBadRequest)
+// 			return
+// 		}
+
+// 		// TODO: identify song
+
+// 		sampleSong := Track{
+// 			Title:         "Never Gonna Give you up",
+// 			Artist:        "Rick Astley",
+// 			DurationS:     rand.IntN(500), // this is just for a tiny bit of variety in the samples
+// 			Album:         "Off the wall",
+// 			AlbumCoverURL: "https://upload.wikimedia.org/wikipedia/en/1/1c/Rick_Astley_-_Whenever_You_Need_Somebody.png",
+// 		}
+
+// 		// broadcast the value to the frontend. returning it to the rust layer making the call will do us no good
+// 		broker.publish(sampleSong)
+// 		w.WriteHeader(http.StatusOK)
+// 	}
+// }
 
 type SSEBroker struct {
 	clients map[chan Track]bool
